a7f408e2d96912a5e29dc19ea60e13bab3ae1196
MarvinAvila <marvinavila2002@gmail.com>
2025-06-06 14:49:32 -0600

    My First Commit

---
+++ RiotShop/src/main/java/com/mycompany/riotshop/RiotShop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.riotshop;
+
+/**
+ *
+ * @author AsHunter
+ */
+public class RiotShop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
