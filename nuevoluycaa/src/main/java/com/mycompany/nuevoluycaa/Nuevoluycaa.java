300d76a5bf1b3aaec895c63ad74cf4a81abc4b44
Guadalupe Orozco <guadalupeorozcohe@gmail.com>
2025-06-06 15:01:30 -0600

    mi primer pareja

--- nuevoluycaa/src/main/java/com/mycompany/nuevoluycaa/Nuevoluycaa.java
+++ nuevoluycaa/src/main/java/com/mycompany/nuevoluycaa/Nuevoluycaa.java
@@ -11,6 +11,6 @@ package com.mycompany.nuevoluycaa;
 public class Nuevoluycaa {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! aaa");
     }
 }
