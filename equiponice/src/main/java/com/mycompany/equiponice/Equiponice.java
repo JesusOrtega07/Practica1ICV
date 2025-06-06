17057b66832c9dbd04daee53791c3263415b7360
BeastGate <raul.gonzalez88@unach.mx>
2025-06-06 15:14:30 -0600

    codigo listo

--- equiponice/src/main/java/com/mycompany/equiponice/Equiponice.java
+++ equiponice/src/main/java/com/mycompany/equiponice/Equiponice.java
@@ -1,12 +1,23 @@
 package com.mycompany.equiponice;
 
+import java.util.Scanner;
 
 public class Equiponice {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        System.out.println("Todo va de maravilla!");
+        Scanner entrada = new Scanner(System.in);
+        
+        int[] numero;
+        numero = new int[10];
+        
+        numero[0] = 0;
+        numero[1] = 1;
+        numero[2] = 2;
+        numero[5] = 5;
+        numero[6] = 6;
+        numero[9] = 9;
+        //# Imprimo el dato de tal pocicion
+        System.out.println("El dato es: " + numero[0] + " y " + numero[9]);
 
     }
 }
