086b83e65e7a0f92d6b2f7e9bef3e0f1cf7b5d49
Yoshiro725 <yoshiro469@gmail.com>
2025-06-06 15:28:20 -0600

    Avance

--- proyectoX/ValeUwU.asm
+++ proyectoX/ValeUwU.asm
@@ -1,13 +1,15 @@
 
-
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 
 org 100h
 
-
+msg db 'Ingrese un numero (0-9): $'
+msg_num db 0Dh,0Ah,'Has ingresado el numero : $'
+msg_fin db 0Dh,0Ah,'Deseas terminar el programa? (s/n): $'
 
 ret
+
 
 
 
