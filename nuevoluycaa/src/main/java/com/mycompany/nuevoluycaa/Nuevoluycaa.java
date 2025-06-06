4dd5bd5d95d4cce59630ea05cdc0e366cc9b7e42
Guadalupe Orozco <guadalupeorozcohe@gmail.com>
2025-06-06 15:21:46 -0600

    clase persona

--- nuevoluycaa/src/main/java/com/mycompany/nuevoluycaa/Nuevoluycaa.java
+++ nuevoluycaa/src/main/java/com/mycompany/nuevoluycaa/Nuevoluycaa.java
@@ -12,5 +12,6 @@ public class Nuevoluycaa {
 
     public static void main(String[] args) {
         System.out.println("Hello World! aaa");
+      
     }
 }
---
+++ nuevoluycaa/src/main/java/com/mycompany/nuevoluycaa/persona.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.nuevoluycaa;
+
+/**
+ *
+ * @author guadalupeorozco
+ */
+public class persona {
+    String nombre,apellidoPA,apellidoMA;
+    int edad ;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPA() {
+        return apellidoPA;
+    }
+
+    public void setApellidoPA(String apellidoPA) {
+        this.apellidoPA = apellidoPA;
+    }
+
+    public String getApellidoMA() {
+        return apellidoMA;
+    }
+
+    public void setApellidoMA(String apellidoMA) {
+        this.apellidoMA = apellidoMA;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public void mostrarDatos() {
+        System.out.println("Mi nombre es " + nombre +""  + apellidoPA+ ""   + apellidoMA+"y tengo"+edad+"años");
+    }
+    
+    
+}
