7dfaaa7d58fa9c7905ab911ddc2318b6cef0f7c5
Grecia Valeria <129807647+GrexValx@users.noreply.github.com>
2025-06-06 15:39:47 -0600

    primera linea

--- proyectoX/ValeUwU.asm
+++ proyectoX/ValeUwU.asm
@@ -1,12 +1,18 @@
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
-
 org 100h
 
 msg db 'Ingrese un numero (0-9): $'
 msg_num db 0Dh,0Ah,'Has ingresado el numero : $'
-msg_fin db 0Dh,0Ah,'Deseas terminar el programa?(s/n): $'
+msg_fin db 0Dh,0Ah,'Deseas terminar el programa?(s/n): $' 
+
+inicio: 
+
+    ; Pide al usuario que ingrese un numero uwu
+    mov ah, 09h
+    lea dx, msg  
+    int 21h   
+
+
 
 ret
 
