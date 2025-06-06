58435dccd2fb3df906a30612a73834424bd81f8e
Grecia Valeria <129807647+GrexValx@users.noreply.github.com>
2025-06-06 15:25:42 -0600

    ya quedo

--- proyectoX/ValeUwU.asm
+++ proyectoX/ValeUwU.asm
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
-
-org 100h
-
-msg db 'Ingrese un numero (0-9): $'
-msg_num db 0Dh,0Ah,'Has ingresado el numero : $'
-msg_fin db 0Dh,0Ah,'Deseas terminar el programa? (s/n): $'
-                                                            
-                                                            //hola
-ret
-
-
-
-
-=======
 
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
@@ -28,5 +11,3 @@ ret
 
 
 
-
->>>>>>> 8845e7c (Primer de Nuevo)
