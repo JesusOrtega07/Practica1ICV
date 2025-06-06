ade2d517c732e7653e23bda04e2971454b96e30a
Yoshiro725 <yoshiro469@gmail.com>
2025-06-06 15:08:23 -0600

    Primer commit

--- proyectoX/ValeUwU.asm
+++ proyectoX/ValeUwU.asm
@@ -7,7 +7,8 @@ org 100h
 msg db 'Ingrese un numero (0-9): $'
 msg_num db 0Dh,0Ah,'Has ingresado el numero : $'
 msg_fin db 0Dh,0Ah,'Deseas terminar el programa? (s/n): $'
-
+                                                            
+                                                            //hola
 ret
 
 
