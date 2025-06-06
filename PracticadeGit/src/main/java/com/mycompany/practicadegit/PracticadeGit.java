dc88497df82c2aeabd8ac2cb5e6af014ab5c524d
alejandro <alejandro.sibaja57@unach.mx>
2025-06-06 15:22:21 -0600

    cambio bueno

---
+++ PracticadeGit/src/main/java/com/mycompany/practicadegit/PracticadeGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicadegit;
+
+/**
+ *
+ * @author aleja
+ */
+public class PracticadeGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello aleanjj!");
+    }
+}
