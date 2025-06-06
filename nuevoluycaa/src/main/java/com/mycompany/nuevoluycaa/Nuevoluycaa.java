ffd7c01be0ac685f6a320f9fdb2ad9fcbc9905d8
Carolina <carolinaverdugo686@gmail.com>
2025-06-06 15:22:56 -0600

    Commit Nuevoluycaamain

--- nuevoluycaa/src/main/java/com/mycompany/nuevoluycaa/Nuevoluycaa.java
+++ nuevoluycaa/src/main/java/com/mycompany/nuevoluycaa/Nuevoluycaa.java
@@ -1,16 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
+//hacer un proyecto de unn  mi programa que guarde el nombre y ya edad 
 package com.mycompany.nuevoluycaa;
 
 /**
  *
  * @author guadalupeorozco
  */
-public class Nuevoluycaa {
+public class Nuevoluycaa extends persona{
 
     public static void main(String[] args) {
-        System.out.println("Hello World! aaa");
+        System.out.println("B I E N V E N I D O S ");
+        persona persona1 = new persona("Ricardo", "Gordillo","Ibarra", 19);
+        persona1.mostrarDatos();
+        
     }
 }
