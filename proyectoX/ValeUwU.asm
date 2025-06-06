e84696190604031d8d412c89d16bbc18caa8562e
Grecia Valeria <129807647+GrexValx@users.noreply.github.com>
2025-06-06 15:31:34 -0600

    grecia hizo algo

--- proyectoX/ValeUwU.asm
+++ proyectoX/ValeUwU.asm
@@ -6,7 +6,7 @@ org 100h
 
 msg db 'Ingrese un numero (0-9): $'
 msg_num db 0Dh,0Ah,'Has ingresado el numero : $'
-msg_fin db 0Dh,0Ah,'Deseas terminar el programa? (s/n): $'
+msg_fin db 0Dh,0Ah,'Deseas terminar el programa?(s/n): $'
 
 ret
 
