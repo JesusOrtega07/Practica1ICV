5ae6c80556e4622c95b8e0224ffc2dd6d608a9c9
MarvinAvila <marvinavila2002@gmail.com>
2025-06-06 23:07:01 -0600

    Lista de Items completa

--- RiotShop/src/main/java/Modelo/ItemList.java
+++ RiotShop/src/main/java/Modelo/ItemList.java
@@ -11,22 +11,42 @@ package Modelo;
 public class ItemList {
 
     public ItemList() {
-
     }
 
     public void mostrarTodos() {
-        Asesino = new Asesino();
+        Asesino as = new Asesino();
+        Luchador top = new Luchador();
+        Tirador adc = new Tirador();
+        Mago Mid = new Mago();
+        Tanque t = new Tanque();
+        Soporte supp = new Soporte();
+
         System.out.println("🥊 Luchador");
+        for (String object : top.OBJETOS) {
+            System.out.println(object);
+        }
 
         System.out.println("\n🎯 Tirador");
-
+        for (String object : adc.OBJETOS) {
+            System.out.println(object);
+        }
         System.out.println("\n🗡️ Asesino");
 
+        for (String object : as.OBJETOS) {
+            System.out.println(object);
+        }
+
         System.out.println("\n🔮 Mago");
-
+        for (String object : Mid.OBJETOS) {
+            System.out.println(object);
+        }
         System.out.println("\n🛡️ Tanque");
-
+        for (String object : t.OBJETOS) {
+            System.out.println(object);
+        }
         System.out.println("\n✨ Soporte");
-
+        for (String object : supp.OBJETOS) {
+            System.out.println(object);
+        }
     }
 }
